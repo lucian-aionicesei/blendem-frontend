312395d401b8bf13f7fa0d2342072f2c1ccf8ce9
Lucian <la@checkmate.dk>
2023-05-20 17:48:47 +0200

    style: created the component - initial commit

---
+++ components/CategoryGrid.tsx
@@ -0,0 +1,9 @@
+const CategoryGrid = () => {
+    return ( 
+        <section>
+            <h1 className=" text-6xl">This is category grif bitch!</h1>
+        </section>
+     );
+}
+ 
+export default CategoryGrid;
