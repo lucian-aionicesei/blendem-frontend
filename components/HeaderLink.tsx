4eb9f4c6c7ce0c2429fc4393e0c85b859653cab6
Lucian <la@checkmate.dk>
2023-05-25 00:19:27 +0200

    style: updated header styling for works path

--- components/HeaderLink.tsx
+++ components/HeaderLink.tsx
@@ -13,14 +13,11 @@ const HeaderLink: React.FC<MyComponentProps> = ({ path, name, burgerMenu = false
     const currentPath = router.pathname;
 
     const isActive = () => {
-        return currentPath === path ? "border-project-green bg-project-green font-bold" : "group";
+        return (currentPath === path || currentPath.includes(`${path}/[slug]`)) ? "border-project-green bg-project-green font-bold" : "group";
     };
 
     return (<>{!burgerMenu ? 
-        <li className={`relative group overflow-hidden ${isActive()}`}><Link href={path} className="block px-4 py-3 ease-in-out duration-300 group-hover:-translate-y-full">{name}</Link><Link href={path} className="block px-4 py-3 absolute group-hover:-translate-y-full ease-in-out duration-300">{name}</Link><span className="absolute left-1/2 bottom-0 border-b-2 w-0 border-project-green transition-all duration-300 ease-in-out group-hover:w-full transform -translate-x-1/2"></span></li> : <li><Link className= {`hover:font-bold ${currentPath === path && 'font-bold text-project-green'}`} onClick={setMenuState} href={path}>{name}</Link></li>}
-        {/* <li>
-            <Link className= {`hover:font-bold ${(currentPath === "/works" || currentPath === "/works/[slug]") && 'font-bold text-project-green'}`} onClick={setMenuState} href="/works">Works</Link>
-        </li> */}
+        <li className={`relative group overflow-hidden ${isActive()}`}><Link href={path} className="block px-4 py-3 ease-in-out duration-300 group-hover:-translate-y-full">{name}</Link><Link href={path} className="block px-4 py-3 absolute group-hover:-translate-y-full ease-in-out duration-300">{name}</Link><span className="absolute left-1/2 bottom-0 border-b-2 w-0 border-project-green transition-all duration-300 ease-in-out group-hover:w-full transform -translate-x-1/2"></span></li> : <li><Link className= {`hover:font-bold ${(currentPath === path || currentPath.includes(`${path}/[slug]`)) && 'font-bold text-project-green'}`} onClick={setMenuState} href={path}>{name}</Link></li>}
     </>
     );
 }
