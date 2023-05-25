bf4382682a7b1f56da6e317f0a2f0813050202cb
George <georgenico9@gmail.com>
2023-05-25 18:29:48 +0200

    refactor: add code formatting file

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,84 +4,142 @@ import { useEffect, useState } from "react";
 import HeaderLink from "./HeaderLink";
 
 const Header = () => {
-    const router = useRouter();
-    const currentPath = router.pathname;
+  const router = useRouter();
+  const currentPath = router.pathname;
 
-    const [toggleMenu, setToggleMenu] = useState(false);
-    const [isScrolled, setIsScrolled] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-
-    useEffect(() => {
-        const handleResize = () => {
-          if (window.innerWidth > 1023) {
-            setToggleMenu(false)
-          }
-        };
-
-		const handleScroll = () => {
-			const scrollPosition = window.scrollY;
-			if (scrollPosition > 0) {
-			  setIsScrolled(true);
-			} else {
-			  setIsScrolled(false);
-			}
-		  };
-	  
-          // Attach event listener for window resize and scroll
-		  window.addEventListener("resize", handleResize);
-		  window.addEventListener("scroll", handleScroll);
-
-          // Clean up the event listener on component unmount
-		  return () => {
-			window.removeEventListener("scroll", handleScroll);
-			window.removeEventListener("resize", handleResize);
-
-		  };
-      }, []);
-
-    function setMenuState() {
-        setToggleMenu(() => !toggleMenu);
-    }
-
-    useEffect(() => {
-      if (!toggleMenu) {
-        document.body.style.overflow = "auto";
-      } else {
-        document.body.style.overflow = "hidden";
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 1023) {
+        setToggleMenu(false);
       }
-    }, [toggleMenu])
+    };
 
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
 
+    // Attach event listener for window resize and scroll
+    window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", handleScroll);
 
-    return ( 
-        <header className="fixed top-0 w-full h-0">
-            <div className={`flex justify-between items-center px-5 md:px-14 h-[5.5rem] z-10 ease-in duration-200 ${(currentPath === "/" && !toggleMenu) ? (isScrolled && "bg-project-black"): ("bg-project-black")}`}>
-                <Link onClick={() => toggleMenu && setMenuState} href="/"><img src="/logo-white.png" className="h-10 w-fit" alt="logo"></img></Link>
-                <nav className="hidden lg:block">
-                    <ul className="flex gap-x-7 uppercase text-base items-center">
-                        <HeaderLink path="/" name="Home"/>
-                        <HeaderLink path="/works" name="Works"/>
-                        <HeaderLink path="/about" name="About"/>
-                        <HeaderLink path="/contact" name="Contact"/>
-                    </ul>
-                </nav>
-                <div onClick={setMenuState} className="block lg:hidden cursor-pointer py-2">
-                    <svg className="w-9 md:w-10 h-fit" viewBox="0 0 38 38" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <rect className={`ease-in-out duration-300 origin-center ${toggleMenu ? "translate-y-0 translate-x-0 rotate-45" : "-translate-y-[6px] -translate-x-[1px]"}`} y="18" x="1" width="38" height="5" fill="white"/>
-                        <rect className={`ease-in-out duration-300 origin-center ${toggleMenu ? "translate-y-0 translate-x-0 -rotate-45" : "translate-y-[6px] translate-x-[1px]"}`} y="18" x="-1" width="38" height="5" fill="white"/>
-                    </svg>
-                </div>
-            </div>
-            <nav className={`backdrop-blur-md bg-black/30 h-screen w-full ml-auto pb-[5.5rem] ease-in-out duration-500 ${toggleMenu ? "translate-x-0" : "translate-x-full"} z-10`}>
-                <ul className="px-5 md:px-14 text-7xl h-full flex flex-col justify-center gap-y-10 text-right">
-                  <HeaderLink path="/" name="Home" setMenuState={setMenuState} burgerMenu={true}/>
-                  <HeaderLink path="/works" name="Works" setMenuState={setMenuState} burgerMenu={true}/>
-                  <HeaderLink path="/about" name="About" setMenuState={setMenuState} burgerMenu={true}/>
-                  <HeaderLink path="/contact" name="Contact" setMenuState={setMenuState} burgerMenu={true}/>
-                </ul>
-            </nav>
-        </header>
-     );
-}
- 
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  function setMenuState() {
+    setToggleMenu(() => !toggleMenu);
+  }
+
+  useEffect(() => {
+    if (!toggleMenu) {
+      document.body.style.overflow = "auto";
+    } else {
+      document.body.style.overflow = "hidden";
+    }
+  }, [toggleMenu]);
+
+  return (
+    <header className="fixed top-0 w-full h-0">
+      <div
+        className={`flex justify-between items-center px-5 md:px-14 h-[5.5rem] z-10 ease-in duration-200 ${
+          currentPath === "/" && !toggleMenu
+            ? isScrolled && "bg-project-black"
+            : "bg-project-black"
+        }`}
+      >
+        <Link onClick={() => toggleMenu && setMenuState} href="/">
+          <img src="/logo-white.png" className="h-10 w-fit" alt="logo"></img>
+        </Link>
+        <nav className="hidden lg:block">
+          <ul className="flex gap-x-7 uppercase text-base items-center">
+            <HeaderLink path="/" name="Home" />
+            <HeaderLink path="/works" name="Works" />
+            <HeaderLink path="/about" name="About" />
+            <HeaderLink path="/contact" name="Contact" />
+          </ul>
+        </nav>
+        <div
+          onClick={setMenuState}
+          className="block lg:hidden cursor-pointer py-2"
+        >
+          <svg
+            className="w-9 md:w-10 h-fit"
+            viewBox="0 0 38 38"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect
+              className={`ease-in-out duration-300 origin-center ${
+                toggleMenu
+                  ? "translate-y-0 translate-x-0 rotate-45"
+                  : "-translate-y-[6px] -translate-x-[1px]"
+              }`}
+              y="18"
+              x="1"
+              width="38"
+              height="5"
+              fill="white"
+            />
+            <rect
+              className={`ease-in-out duration-300 origin-center ${
+                toggleMenu
+                  ? "translate-y-0 translate-x-0 -rotate-45"
+                  : "translate-y-[6px] translate-x-[1px]"
+              }`}
+              y="18"
+              x="-1"
+              width="38"
+              height="5"
+              fill="white"
+            />
+          </svg>
+        </div>
+      </div>
+      <nav
+        className={`backdrop-blur-md bg-black/30 h-screen w-full ml-auto pb-[5.5rem] ease-in-out duration-500 ${
+          toggleMenu ? "translate-x-0" : "translate-x-full"
+        } z-10`}
+      >
+        <ul className="px-5 md:px-14 text-7xl h-full flex flex-col justify-center gap-y-10 text-right">
+          <HeaderLink
+            path="/"
+            name="Home"
+            setMenuState={setMenuState}
+            burgerMenu={true}
+          />
+          <HeaderLink
+            path="/works"
+            name="Works"
+            setMenuState={setMenuState}
+            burgerMenu={true}
+          />
+          <HeaderLink
+            path="/about"
+            name="About"
+            setMenuState={setMenuState}
+            burgerMenu={true}
+          />
+          <HeaderLink
+            path="/contact"
+            name="Contact"
+            setMenuState={setMenuState}
+            burgerMenu={true}
+          />
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,23 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  // asdsa
   return (
-      <main>
-        <img className=' h-screen w-full object-cover' src="/mtbike.png" alt="logo"></img>
-        <img className=' h-screen w-full object-cover' src="/mtbike.png" alt="logo"></img>
-      </main>
-  )
+    <main>
+      <img
+        className=" h-screen w-full object-cover"
+        src="/mtbike.png"
+        alt="logo"
+      ></img>
+      <img
+        className=" h-screen w-full object-cover"
+        src="/mtbike.png"
+        alt="logo"
+      ></img>
+    </main>
+  );
 }
