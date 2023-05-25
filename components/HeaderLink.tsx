889e10b3417f9978508146e15052948c7ce098b8
George <georgenico9@gmail.com>
2023-05-25 18:18:36 +0200

    fix: small change to the HeroLink component

--- components/HeaderLink.tsx
+++ components/HeaderLink.tsx
@@ -8,16 +8,14 @@ interface MyComponentProps {
     setMenuState?: () => void;
   }
 
-const HeaderLink: React.FC<MyComponentProps> = ({ path, name, burgerMenu = false, setMenuState }) => {
+const HeaderLink = ({ path, name, burgerMenu = false, setMenuState }: MyComponentProps) => {
     const router = useRouter();
     const currentPath = router.pathname;
 
-    const isActive = () => {
-        return currentPath === path ? "border-project-green bg-project-green font-bold" : "group";
-    };
+    const isActive = currentPath === path ? "border-project-green bg-project-green font-bold" : "group";
 
     return (<>{!burgerMenu ? 
-    <li className="relative"><Link href={path} className={`block px-4 py-3 ${isActive()}`}>{name}<span className="absolute left-1/2 bottom-0 border-b-2 w-0 border-project-green transition-all duration-300 ease-in-out group-hover:w-full transform -translate-x-1/2"></span></Link></li> : <li><Link className= {`hover:font-bold ${currentPath === path && 'font-bold text-project-green'}`} onClick={setMenuState} href={path}>{name}</Link></li>}
+    <li className="relative"><Link href={path} className={`block px-4 py-3 ${isActive}`}>{name}<span className="absolute left-1/2 bottom-0 border-b-2 w-0 border-project-green transition-all duration-300 ease-in-out group-hover:w-full transform -translate-x-1/2"></span></Link></li> : <li><Link className= {`hover:font-bold ${currentPath === path && 'font-bold text-project-green'}`} onClick={setMenuState} href={path}>{name}</Link></li>}
     </>
     );
 }
